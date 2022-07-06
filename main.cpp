778479fcb2c9ee36ddc51d6e18afbaf2ddd026ee
jihun <jhk3763@naver.com>
2022-07-06 22:42:09 +0900

    Level2 배달

--- main.cpp
+++ main.cpp
@@ -1,33 +1,47 @@
-#include<string>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include<queue>
 using namespace std;
-
-int visited[10000];
-string ans_string = "a";
-
-void dfs(vector<vector<string>>& tickets, string cur, string path, int depth) {
-    if (depth == tickets.size()) {
-        string p = path;
-        if (path < ans_string) {
-            ans_string = path;
-        }
-        return;
-    }
-
-    for (int i = 0; i < tickets.size(); i++) {
-        if (cur == tickets[i][0] && !visited[i]) {
-            visited[i] = 1;
-            dfs(tickets, tickets[i][1], path + tickets[i][1], depth + 1);
-            visited[i] = 0;
+vector<int>dist(51,10000000000);
+void dijkstra(vector<vector<int>>MAP,int start,int K){
+    priority_queue<pair<int,int>>q;
+    q.push({0,start});
+    dist[start]=0;
+    while(!q.empty()){
+        int loc=q.top().second;
+        int cost=-q.top().first;
+        q.pop();
+        for(int i=1;i<MAP[loc].size();i++){
+            if(MAP[loc][i]!=0&&MAP[loc][i]+cost<=K&&dist[i]>MAP[loc][i]+cost){
+                dist[i]=MAP[loc][i]+cost;
+                q.push({-dist[i],i});
+            }
         }
     }
 }
-vector<string>solution(vector<vector<string>>tickets) {
-    vector<string> answer;
-    dfs(tickets, "ICN", "ICN", 0);
-    for (int i = 0; i < ans_string.size(); i += 3) {
-        answer.push_back(ans_string.substr(i, 3));
+int solution(int N, vector<vector<int> > road, int K) {
+    int answer = 0;
+    vector<vector<int>> MAP(N + 1, vector<int>(N + 1));
+    for (int i = 0; i < road.size(); i++) {
+        if(MAP[road[i][0]][road[i][1]]==0){
+            MAP[road[i][0]][road[i][1]] = road[i][2];
+            MAP[road[i][1]][road[i][0]] = road[i][2];
+        }
+        else{
+            if(MAP[road[i][0]][road[i][1]]<road[i][2]){
+                continue;
+            }
+            else{
+                MAP[road[i][0]][road[i][1]] = road[i][2];
+                MAP[road[i][1]][road[i][0]] = road[i][2];
+            }
+        }
+    }
+    dijkstra(MAP, 1, K);
+    for(int i=1;i<N+1;i++){
+        if(dist[i]<=K){
+            answer++;
+        }
     }
     return answer;
 }
